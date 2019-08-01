5551c7cc31711ca3378e2d08223cafd90b3c5137
Sho Tanaka <twinkle.over.the.sky@gmail.com>
2019-08-02 03:42:11 +0900

    Add ch03 ex12

---
+++ ch03/ex12/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+func main() {
+	if len(os.Args) != 3 {
+		fmt.Fprintln(os.Stderr, "must have 2 arguments.")
+		os.Exit(1)
+	}
+	fmt.Printf("%t\n", anagram(os.Args[1], os.Args[2]))
+}
+
+func anagram(a, b string) bool {
+	return equals(runeOccurrences(a), runeOccurrences(b))
+}
+
+func runeOccurrences(s string) map[rune]int {
+	occurrences := make(map[rune]int)
+	for _, r := range s {
+		occurrences[r]++
+	}
+	return occurrences
+}
+
+func equals(a, b map[rune]int) bool {
+	if len(a) != len(b) {
+		return false
+	}
+	for k := range a {
+		if a[k] != b[k] {
+			return false
+		}
+	}
+	return true
+}
